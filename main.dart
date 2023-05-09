0c131498fb4e537f54d120c79d82a5dccc0a85cd
molham khudier <132438008+molham1996@users.noreply.github.com>
2023-05-09 08:42:24 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dependent DropDown',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Dependent DropDown'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _selectedCountry = "syria";
+  var country = {'syria': 'sy', 'lebnon': 'leb', 'iraq': 'ir', 'egypt': 'eg'};
+
+  List _countries = [];
+  CountryDependentDropDown() {
+    country.forEach((key, value) {
+      _countries.add(key);
+    });
+  }
+
+  String _selectedState = "";
+  var state = {
+    'الاتحاد': 'sy',
+    'الكرامة': 'sy',
+    'Baluchistan': 'leb',
+    'الاهلي': 'eg',
+    'العراقي': 'ir',
+    'الزملكاوي': 'eg'
+  };
+
+  List _states = [];
+  StateDependentDropDown(countryShortName) {
+    state.forEach((key, value) {
+      if (countryShortName == value) {
+        _states.add(key);
+      }
+    });
+    _selectedState = _states[0];
+  }
+
+  String _selectedCity = "";
+  var city = {
+    'red': 'الاتحاد',
+    'blue': 'الكرامة',
+    'اخضر': 'العراقي',
+    'احمر': 'الزملكاوي',
+    'ابيض': 'الاهلي'
+  };
+
+  List _cities = [];
+  CityDependentDropDown(stateShortName) {
+    city.forEach((key, value) {
+      if (stateShortName == value) {
+        _cities.add(key);
+      }
+    });
+    _selectedCity = _cities[0];
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    CountryDependentDropDown();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 23,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Country",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            Container(
+              width: 400,
+              child: DropdownButton(
+                value: _selectedCountry,
+                onChanged: (newValue) {
+                  setState(() {
+                    _cities = [];
+                    _states = [];
+                    StateDependentDropDown(country[newValue]);
+                    _selectedCountry = "$newValue";
+                  });
+                },
+                items: _countries.map((country) {
+                  return DropdownMenuItem(
+                    child: new Text(country),
+                    value: country,
+                  );
+                }).toList(),
+              ),
+            ),
+            SizedBox(
+              height: 23,
+            ),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "items",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                )),
+            Container(
+              width: 400,
+              child: DropdownButton(
+                value: _selectedState,
+                onChanged: (newValue) {
+                  print(newValue);
+                  setState(() {
+                    print(newValue);
+                    _cities = [];
+                    CityDependentDropDown(newValue);
+
+                    _selectedState = "$newValue";
+                  });
+                },
+                items: _states.map((state) {
+                  return DropdownMenuItem(
+                    child: new Text(state),
+                    value: state,
+                  );
+                }).toList(),
+              ),
+            ),
+            SizedBox(
+              height: 23,
+            ),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "color",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                )),
+            Container(
+              width: 400,
+              child: DropdownButton(
+                value: _selectedCity,
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedCity = "$newValue";
+                  });
+                },
+                items: _cities.map((city) {
+                  return DropdownMenuItem(
+                    child: new Text(city),
+                    value: city,
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
